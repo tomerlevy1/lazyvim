26db62f1407fe6ac98514cb76252b6c30b05602f
Tomer <tolevy@outbrain.com>
2024-11-21 16:37:28 +0200

    experimenting with cmp/luasnip and copilot

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,50 +1,48 @@
-local cmp = require("cmp")
+-- local cmp = require("cmp")
+-- local luasnip = require("luasnip")
 
+-- checking the copilot is playing nice with luasnip
 return {
   {
     "nvim-cmp",
-    opts = {
-      mapping = cmp.mapping.preset.insert({
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      print(vim.inspect(opts.mapping))
+      opts.mapping = cmp.mapping.preset.insert({
         ["<Tab>"] = cmp.mapping(function(fallback)
           local copilot_available, copilot_suggestion = pcall(require, "copilot.suggestion")
           -- Checks if copilot is visible, and accept completion if it is
           -- print(vim.inspect(copilot_suggestion))
 
-          if copilot_available and copilot_suggestion.is_visible() then
+          if luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          elseif copilot_available and copilot_suggestion.is_visible() then
             copilot_suggestion.accept()
           else
             fallback()
           end
         end, { "i", "s" }),
-      }),
-    },
-    -- opts = function(_, opts)
-    --   local cmp = require("cmp")
-    --   local opts_win = {
-    --     window = {
-    --       completion = cmp.config.window.bordered(),
-    --       documentation = cmp.config.window.bordered(),
-    --     },
-    --     experimental = { ghost_text = false },
-    --     mapping = cmp.mapping.preset.insert({
-    --       ["<Tab>"] = cmp.mapping(function(fallback)
-    --         local copilot_available, copilot_suggestion = pcall(require, "copilot.suggestion")
-    --         -- Checks if copilot is visible, and accept completion if it is
-    --         -- print(vim.inspect(copilot_suggestion))
+      })
+      return opts
+    end,
+    -- opts = {
+    --   mapping = cmp.mapping.preset.insert({
+    --     ["<Tab>"] = cmp.mapping(function(fallback)
+    --       local copilot_available, copilot_suggestion = pcall(require, "copilot.suggestion")
+    --       -- Checks if copilot is visible, and accept completion if it is
+    --       -- print(vim.inspect(copilot_suggestion))
     --
-    --         if copilot_available and copilot_suggestion.is_visible() then
-    --           copilot_suggestion.accept()
-    --         else
-    --           fallback()
-    --         end
-    --       end, { "i", "s" }),
-    --     }),
-    --   }
-    --
-    --   -- vim.print(vim.inspect(opts.mapping))
-    --   local new_opts = vim.tbl_extend("force", opts, opts_win)
-    --   -- vim.print(vim.inspect(new_opts.keys))
-    --   return new_opts
-    -- end,
+    --       if luasnip.expand_or_locally_jumpable() then
+    --         luasnip.expand_or_jump()
+    --       elseif copilot_available and copilot_suggestion.is_visible() then
+    --         copilot_suggestion.accept()
+    --       else
+    --         fallback()
+    --       end
+    --     end, { "i", "s" }),
+    --   }),
+    -- },
   },
 }
